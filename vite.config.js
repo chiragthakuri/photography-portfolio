c1ce1e4d65fbbf520ca09bb844bd4e0ef28dc30e
Chirag <chiragbikram@gmail.com>
2025-04-16 18:01:42 +1000

    Setup preview deploy for dev branch

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Use environment variable to set base path
+const base = process.env.BASE_PATH || '/photography-portfolio/'
+
 export default defineConfig({
-  base: '/photography-portfolio/', // ← this is the key!
+  base,
   plugins: [react()],
 })
